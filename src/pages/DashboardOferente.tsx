6efcd5fc669796b88a1449d71ccb98a115a3effe
Benjamin <benjaminreciolopez@hotmail.es>
2025-05-18 18:26:35 +0200

    Modo mantenimiento

--- src/pages/DashboardOferente.tsx
+++ src/pages/DashboardOferente.tsx
@@ -838,8 +838,8 @@ const DashboardOferente: React.FC = () => {
                     );
                     return (
                       <option key={sol.solicitud_id} value={sol.solicitud_id}>
-                        {sol.cliente_nombre} —
-                        {usuario?.especialidad || "Sin especialidad"}
+                        {sol.cliente_nombre} —{" "}
+                        {datosSolicitud?.especialidad || "Sin especialidad"}
                       </option>
                     );
                   })}
